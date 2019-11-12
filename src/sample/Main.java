5cde7908ce76a4f75621f7e10d50b73db47316d0
theRealScoobaSteve <stephensimone@live.com>
2019-11-11 23:21:26 -0800

    add complete landing screen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Manager");
-        primaryStage.setScene(new Scene(root, 1000, 275));
+        primaryStage.setScene(new Scene(root, 1200, 500));
         primaryStage.show();
     }
 
